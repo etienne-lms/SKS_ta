e99156724bd3edd1c3cf3e48d36c9e4e0f5ab1d5
Etienne Carriere <etienne.carriere@linaro.org>
2017-12-07 21:34:36 +0100

    debug: fill cka2str() with all attributes
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- src/ck2tee_id.h
+++ src/ck2tee_id.h
@@ -12,9 +12,16 @@
 #include <tee_internal_api.h>
 #include <tee_internal_api_extensions.h>
 
+/*
+ * Convert a CK return value ID into a TEE Internal Core API return value ID.
+ * This supports only a reduced set of IDs as such conversion is prone to
+ * error. Supported IDs are CKR_OK, CKR_TOO_SMALL_BUFFER, CKR_DEVICE_MEMORY,
+ * etc...
+ */
 TEE_Result ckr2tee(CK_RV rv);
 
-const char *cka2str(uint32_t class);
+/* Return a pointer to a string buffer of "CKA_xxx" attribute ID */
+const char *cka2str(uint32_t id);
 
 #endif /*__CK2TEE_ID_H*/
 
