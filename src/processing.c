6cbccc5faedae19ef8a17c72a724ced0bba8d90c
Etienne Carriere <etienne.carriere@linaro.org>
2017-12-11 11:06:18 +0100

    processing: split ciphering sequence
    Call key_matches_cipher() to check key against crypto algorithm. There
    are still many attributes that should be checked from here.
    
    Call tee_operarion_params() to get TEE operation parameters from
    key and mechanism info.

--- src/processing.c
+++ src/processing.c
@@ -17,6 +17,102 @@
 #define DUMMY_NULL_AES_IV_SIZE		16
 static const char dummy_null_aes_iv[DUMMY_NULL_AES_IV_SIZE];
 
+/* TODO: get a Cryptoki return value out of this */
+static bool key_matches_cipher(CK_MECHANISM_PTR ck_mechanism,
+				  struct sks_key_object *sks_key,
+				  bool decrypt)
+{
+	uint32_t key_class;
+	uint32_t key_type;
+
+	/* expect a class and a type */
+	if (serial_get_attribute(sks_key->attributes, CKA_CLASS,
+				 &key_class, NULL) ||
+	    serial_get_attribute(sks_key->attributes, CKA_KEY_TYPE,
+				  &key_type, NULL))
+		return false;
+
+	/* Check key against mechanism */
+	if (key_class != CKO_SECRET_KEY)
+		return false;
+
+	switch (key_type) {
+	case CKK_AES:
+		switch (ck_mechanism->mechanism) {
+		case CKM_AES_ECB:
+		case CKM_AES_CBC:
+			break;
+		case CKM_AES_CBC_PAD:
+		case CKM_AES_CTS:
+		case CKM_AES_CTR:
+		case CKM_AES_GCM:
+		case CKM_AES_CCM:
+		default:
+			return false;
+		}
+		break;
+	default:
+		return false;
+	}
+
+	if (!serial_boolean_attribute_matches(sks_key->attributes, decrypt ?
+					      CKA_DECRYPT : CKA_ENCRYPT,
+					      CK_TRUE))
+		return false;
+
+	/* TODO: lots of other attributes.... */
+	return true;
+}
+
+struct tee_operation_params {
+	uint32_t algo;
+	uint32_t mode;
+	uint32_t size;
+};
+
+/* TODO: get a Cryptoki return value out of this */
+static TEE_Result tee_operarion_params(struct tee_operation_params *params,
+				CK_MECHANISM_PTR ck_mechanism,
+				struct sks_key_object *sks_key,
+				bool decrypt)
+{
+	uint32_t key_type;
+
+	if (serial_get_attribute(sks_key->attributes, CKA_KEY_TYPE,
+				  &key_type, NULL))
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	switch (key_type) {
+	case CKK_AES:
+		params->mode = decrypt ? TEE_MODE_DECRYPT : TEE_MODE_ENCRYPT;
+		params->size = 16; // TODO: get size from the key attributes
+
+		switch (ck_mechanism->mechanism) {
+		case CKM_AES_ECB:
+			params->algo = TEE_ALG_AES_ECB_NOPAD;
+			break;
+		case CKM_AES_CBC:
+			params->algo = TEE_ALG_AES_CBC_NOPAD;
+			break;
+		case CKM_AES_CBC_PAD:
+			return TEE_ERROR_NOT_SUPPORTED;
+		case CKM_AES_CTS:
+		case CKM_AES_CTR:
+		case CKM_AES_GCM:
+		case CKM_AES_CCM:
+			return TEE_ERROR_NOT_SUPPORTED;
+		default:
+			return TEE_ERROR_BAD_PARAMETERS;
+		}
+		break;
+	default:
+		return TEE_ERROR_NOT_SUPPORTED;
+	}
+
+	return TEE_SUCCESS;
+}
+
+
 /*
  * ctrl = [session-handle][key-handle][serialized-mechanism-blob]
  * in = none
@@ -27,7 +123,6 @@ TEE_Result entry_cipher_init(TEE_Param *ctrl,
 				TEE_Param *out,
 				int decrypt)
 {
-	CK_RV rv;
 	TEE_Result res;
 	uint32_t session;
 	uint32_t key_handle;
@@ -35,23 +130,17 @@ TEE_Result entry_cipher_init(TEE_Param *ctrl,
 	size_t ctrl2_size = ctrl->memref.size;
 	struct sks_key_object *sks_key;
 	CK_MECHANISM ck_mechanism;
-	CK_KEY_TYPE key_type;
 	struct pkcs11_session *pkcs_session;
-	uint32_t tee_algo;
-	uint32_t tee_algo_mode;
-	size_t key_size;
+	struct tee_operation_params tee_op_params;
+	uint32_t key_type;
 
+	/* Arguments */
 	if (!ctrl || in || out || ctrl->memref.size < 2 * sizeof(uint32_t))
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	session = *(uint32_t *)(void *)ctrl2;
 	ctrl2 += sizeof(uint32_t);
 	ctrl2_size -= sizeof(uint32_t);
-
-	/* Update session state */
-	if (set_pkcs_session_processing_state(session,
-					      PKCS11_SESSION_ENCRYPTING))
-		return TEE_ERROR_BAD_STATE;
 
 	pkcs_session = get_pkcs_session(session);
 	if (!pkcs_session) {
@@ -69,63 +158,33 @@ TEE_Result entry_cipher_init(TEE_Param *ctrl,
 		goto error;
 	}
 
-	rv = serial_get_attribute(sks_key->attributes, CKA_KEY_TYPE,
-				  &key_type, NULL);
-	if (rv)
-		TEE_Panic(0);
+	TEE_MemMove(&ck_mechanism, ctrl2, sizeof(ck_mechanism));
 
-
-	memcpy(&ck_mechanism, ctrl2, sizeof(ck_mechanism));
-
-	/* Check key main attribute (class/type) match algorithm */
-	switch (key_type) {
-	case CKK_AES:
-		tee_algo_mode = decrypt ? TEE_MODE_DECRYPT : TEE_MODE_ENCRYPT;
-		key_size = 16; // TODO: get size from the key attributes
-
-		switch (ck_mechanism.mechanism) {
-		case CKM_AES_ECB:
-			tee_algo = TEE_ALG_AES_ECB_NOPAD;
-			break;
-		case CKM_AES_CBC:
-			tee_algo = TEE_ALG_AES_CBC_NOPAD;
-			break;
-		case CKM_AES_CBC_PAD:
-			res = TEE_ERROR_NOT_SUPPORTED;
-			goto error;
-		case CKM_AES_CTS:
-		case CKM_AES_CTR:
-		case CKM_AES_GCM:
-		case CKM_AES_CCM:
-			res = TEE_ERROR_NOT_SUPPORTED;
-			goto error;
-		default:
-			res = TEE_ERROR_BAD_PARAMETERS;
-			goto error;
-		}
-		break;
-	default:
-		return TEE_ERROR_NOT_SUPPORTED;
+	/* Check states */
+	if (set_pkcs_session_processing_state(session,
+					      PKCS11_SESSION_ENCRYPTING)) {
+		res = TEE_ERROR_BAD_STATE;
+		goto error;
 	}
 
-	/* Check the key attributes */
-	if (!serial_boolean_attribute_matches(sks_key->attributes, decrypt ?
-					      CKA_DECRYPT : CKA_ENCRYPT,
-					      CK_TRUE)) {
-		/* TODO: expected CK retval CKR_KEY_FUNCTION_NOT_PERMITTED */
-		MSG("Operation not permited: key is not allowed for %scryption",
-						decrypt ? "de" : "en");
+	if (!key_matches_cipher(&ck_mechanism, sks_key, decrypt)) {
 		res = TEE_ERROR_BAD_PARAMETERS;
 		goto error;
 	}
 
+	/* Allocate a TEE operation for the target processing */
+	res = tee_operarion_params(&tee_op_params, &ck_mechanism, sks_key,
+				   decrypt);
+	if (res)
+		goto error;
+
 	if (pkcs_session->tee_op_handle != TEE_HANDLE_NULL)
 		TEE_Panic(0);
 
-	/* Allocate operation: AES/CTR, mode and size from params */
 	res = TEE_AllocateOperation(&pkcs_session->tee_op_handle,
-				    tee_algo, tee_algo_mode, key_size * 8);
-	if (res != TEE_SUCCESS) {
+				    tee_op_params.algo, tee_op_params.mode,
+				    tee_op_params.size * 8);
+	if (res) {
 		EMSG("Failed to allocate operation");
 		goto error;
 	}
@@ -139,6 +198,10 @@ TEE_Result entry_cipher_init(TEE_Param *ctrl,
 
 
 	/* Specifc cipher initialization */
+	if (serial_get_attribute(sks_key->attributes, CKA_KEY_TYPE,
+				  &key_type, NULL))
+		TEE_Panic(0);
+
 	switch (key_type) {
 	case CKK_AES:
 		switch (ck_mechanism.mechanism) {
