3a29997fbfa058674e17ce8afe21e7d457747a63
Etienne Carriere <etienne.carriere@linaro.org>
2017-12-13 15:53:30 +0100

    ck_debug: complete attribute list and add class and type strings
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- src/ck_debug.h
+++ src/ck_debug.h
@@ -10,10 +10,10 @@
 #include <tee_internal_api.h>
 #include <tee_internal_api_extensions.h>
 
-/* Return a pointer to a string buffer of "CKA_xxx\0" attribute ID */
+/* Return a pointer to a string buffer "CKA_xxx\0" for an attribute ID */
 const char *cka2str(CK_ATTRIBUTE_TYPE id);
 
-/* Return a pointer to a string buffer of "CKR_xxx\0" attribute ID */
+/* Return a pointer to a string buffer "CKR_xxx\0" for a return value ID */
 const char *ckr2str(CK_RV id);
 
 /*
@@ -24,12 +24,18 @@ const char *ckr2str(CK_RV id);
  */
 TEE_Result ckr2tee(CK_RV rv);
 
-/* Return a pointer to a string buffer of "SKS_TA_CMD_xxx\0" */
+/* Return a pointer to a string buffer "SKS_TA_CMD_xxx\0" for a command ID */
 const char *skscmd2str(uint32_t id);
 
-/* Allocate and return a string descripbing the enabled flags */
+/* Allocate and return a string describing the enabled flags */
 char *ck_slot_flag2str(CK_ULONG flags);
 char *ck_token_flag2str(CK_ULONG flags);
 char *ck_mecha_flag2str(CK_ULONG flags);
+
+/* Return a pointer to a string buffer "CKO_xxx\0" for a object class ID */
+const char *ckclass2str(CK_ULONG id);
+
+/* Return a pointer to a string buffer "CKx_xxx\0" for a type-in-class ID */
+const char *cktype2str(CK_ULONG id, CK_ULONG class);
 
 #endif /*__SKS_CK_DEBUG_H*/
