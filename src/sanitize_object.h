2f502bcdd6adc5eafc433034de561a75540163d2
Etienne Carriere <etienne.carriere@linaro.org>
2017-12-21 00:35:24 +0100

    sanitize: minor comment fixup
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- src/sanitize_object.h
+++ src/sanitize_object.h
@@ -9,7 +9,7 @@
 #include <pkcs11.h>
 #include "serializer.h"
 
-/* Generate a sanitzed vopy of a serialized CK attribute object */
+/* Generate a sanitized copy of a serialized CK attribute object */
 CK_RV serial_sanitize_attributes(void **head, void *ref, size_t ref_size);
 
 /* TODO */
